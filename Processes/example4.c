e702b198ca9e75124ad90c797111beb24637e120
Asif Rasheed <asif@linuxmail.org>
2020-09-29 21:51:55 +0400

    Modified Example 4
    Example 4 now takes parameter from shell, prints it line by line using shared memory.
    
    Singed-off-by: Asif Rasheed

--- Processes/example4.c
+++ Processes/example4.c
@@ -1,14 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<string.h>
 #include<fcntl.h>
 #include<sys/mman.h>
 #include<sys/stat.h>
 
-int main(){
-	int fd = shm_open("asif",O_CREAT,0666);
-	ftruncate(fd,1024);
-	void* ptr = mmap(0,1024,PROT_WRITE,MAP_SHARED,fd,0);
-	sprintf(ptr,"The quick brown fox jumps over the lazy dog");
+int main(int argc, char** argv){
+	if(argc==1){
+		printf("Usage ./program [argument]\n");
+		exit(1);
+	}
+
+	int fd = shm_open("asif",O_CREAT|O_RDWR,0666);
+	ftruncate(fd,strlen(argv[1]));
+	void* ptr = mmap(0,1024,PROT_WRITE|PROT_READ,MAP_SHARED,fd,0);
+	sprintf(ptr,argv[1]);
+
+	unsigned char* f = (char *) ptr;
+	for(int i=0;i<strlen(argv[1]);i++){
+		char out = f[i];
+		printf("%c\n",out);
+	}
+
+	shm_unlink("asif");
 	return 0;
 }
