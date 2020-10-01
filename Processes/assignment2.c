421173173043cd208d54c39a56b489808cf26163
Asif Rasheed <b00073877@aus.edu>
2020-10-01 12:44:04 +0400

    Modified assignment2.c and Added Makefile
    I added a make file so that the file compiles to the name monte.
    I deleted the old assignment2.c file and wrote the new one from the beginning just to make sure that its clean, please go through the code and see if it has any issues. It runs fine on my machine.
    
    Signed-off-by: Asif Rasheed <asif@linuxmail.org>

--- Processes/assignment2.c
+++ Processes/assignment2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
 #include<unistd.h>
+#include<math.h>
 #include<fcntl.h>
 #include<sys/mman.h>
 #include<sys/stat.h>
@@ -9,52 +9,54 @@
 #include<sys/time.h>
 
 /*
- Program to calculate pi approximations using Monte Carlo method. Written by Abdullah Siddiqui (00075201) and Asif Rasheed (00073877).
+ Program to approximate the value of pi using Monte Carlo method. Written by Abdullah Siddiqui (00075201) and Asif Rasheed (00073877).
  The pagram accepts number of child processes and number of sample points used for the approximation as command line parameters.
+ The program uses pipes for communication between the child processes and the parent process.
+ The program uses time based on the UNIX Epoch to calculate the speedof time for each child process.
  */
 
 int main(int argc, char** argv){
-    // If not all command line parameters are passed
     if(argc < 3){
         printf("Usage: ./monte children points\n");
         exit(1);
     }
     
+    int fd[2];
+    if(pipe(fd) == -1){
+        printf("Failed to create pipe\n");
+        exit(1);
+    }
+    
     const int NUM_CHILD = atoi(argv[1]); // Number of child processes
     const int NUM_POINT = atoi(argv[2]); // Number of sample points
-    int ctotal = 0; // Sum of number of points inside the circle calculated by each child
-    double pi[NUM_CHILD]; // Array to store pi approximations for each child
-    double runtime[NUM_CHILD]; // Array to store run time of each child in milliseconds
-    double totaltime = 0; // Total runtime of all the child processes
+    int ctotal = 0; // Sum total of the number of sample points inside the circle calculated by each child prrocess
+    double total_runtime; // Approximate total runtime of the program
     
-    int count = 0; // Number of sample points assigned to child processes. Once all child processes are assigned sample points, count = NUM_POINT
+    struct timespec start_time, end_time; // Used to approximate the runtime of the program
+    clock_gettime(CLOCK_REALTIME, &start_time);
+    
+    int count = 0; // Number of sample points assigned to the child processes. Once all child processes are assigned sample points, count = NUM_POINT
     for(int i=0; i<NUM_CHILD; i++){
-        int fd[2];
-        
-        // If pipe fails the program prints fail message and terminates with status 1
-        if(pipe(fd) == -1){
-            printf("Failed to create pipe!\n");
-            exit(1);
-        }
-        
         const int child_points = (i == NUM_CHILD-1) ? NUM_POINT-count : ceil((double) NUM_POINT/NUM_CHILD); // To balance the number of sample points assigned to each child (if NUM_POINT is not a multiple of NUM_CHILD)
-        
         count += child_points;
         
         pid_t pid = fork();
         
         if(pid<0){
-            printf("Fork Failed at %d\n", i);
+            printf("Fork Failed for Child %d\n", i+1);
             exit(1);
         }
         /*
-         Each child will generate x and y coordinates for the number of sample points assigned to it.
+         Each child process will generate x and y coordinates for the number of sample points assigned to it.
          If the distance between the point and origin is less than or equal to 1, it is considered to be inside the circle.
-         From the sample points, the child counts the number of points inside the circle (stored in cpoints) and return it to the parent through a pipe.
-         */
+         From the sample points, the child process will count the number of points inside the circle (stored in cpoints).
+         The child process returns its pid, number of points inside the circle, total number of points assigned to it and the total runtime (in nano seconds) to the parent using pipe.
+        */
         else if(pid == 0){
+            struct timespec child_start_time, child_end_time;
             int cpoints = 0;
             
+            clock_gettime(CLOCK_REALTIME, &child_start_time);
             for(int i = 0; i<child_points; i++){
                 double x = -1 + ((double) rand() / RAND_MAX) * 2;
                 double y = -1 + ((double) rand() / RAND_MAX) * 2;
@@ -62,48 +64,47 @@ int main(int argc, char** argv){
                 if(sqrt(pow(x,2)+pow(y,2))<=1)
                     ++cpoints;
             }
+            clock_gettime(CLOCK_REALTIME, &child_end_time);
             
             FILE* ptr = fdopen(fd[1], "w");
-            fprintf(ptr, "%d\n", cpoints);
+            fprintf(ptr, "%d %d %d %d\n", getpid(), cpoints, child_points, abs(end_time.tv_nsec - start_time.tv_nsec));
             fclose(ptr);
             
             exit(0);
         }
-        /*
-         Parent waits for child to terminate.
-         Parent calculates the time elapsed while waiting for the child (which is approximately the runtime of the child process as the time is calculated with respect to the Epoch) and stores it in runtime.
-         Parent adds the run time of the child process to totaltime.
-         Once child terminates, the parent reads the number of sample points inside the circle from the pipe.
-         Using the number of sample points inside the circle and the total number of points assigned to the child (total number of points assigned to the child = total number of points in a rectangle) calculates the value of pi.
-         The value of pi is store in the array pi.
-         The number of sample points in the circle is added to ctotal.
-         */
-        else{
-            struct timespec child_start_time, child_end_time;
-            
-            clock_gettime(CLOCK_REALTIME, &child_start_time);
-            wait(NULL);
-            clock_gettime(CLOCK_REALTIME, &child_end_time);
-            
-            runtime[i] = abs(child_end_time.tv_nsec - child_start_time.tv_nsec) * 1e-9;
-            totaltime += runtime[i];
-                        
-            char cpoints[256];
-            FILE* ptr = fdopen(fd[0], "r");
-            fscanf(ptr, "%s", cpoints);
-            fclose(ptr);
-            
-            ctotal += atoi(cpoints);
-            pi[i] = (double) 4 * atoi(cpoints) / child_points;
-        }
     }
     
-    // Prints all the pi approximations and speedup time for each child process
+    // Waits for all the child processes to terminate
+    for(int i=0; i<NUM_CHILD; i++)
+        wait(NULL);
+    
+    clock_gettime(CLOCK_REALTIME, &end_time);
+    total_runtime = abs(end_time.tv_nsec - start_time.tv_nsec) * 1e-9;
+    
+    char cpid[256]; // Process id of the child process
+    char cpoints[256]; // Number of points inside the circle out of the total number of sample points assigned to the child process
+    char spoints[256]; // Number of sample points assigned to the child process
+    char runtime[256]; // Total runtime of the child process in nanoseconds
+    
+    FILE* ptr = fdopen(fd[0], "r");
+    /*
+     Calculates and prints the pi approximation using the number of sample points assigned to the child process and the number of points inside the circle out of the total number of sample points assigned for each child point.
+     Converts the runtime of the child process from nanoseconds to seconds, calculates and prints the speedup time for each child process.
+     */
     for(int i=0; i<NUM_CHILD; i++){
-        printf("Pi Approximation %d = %f\n", i+1, pi[i]);
-        printf("Speed up time for child %d = %f\n\n", i+1, runtime[i]/totaltime);
+        fscanf(ptr, "%s", cpid);
+        fscanf(ptr, "%s", cpoints);
+        fscanf(ptr, "%s", spoints);
+        fscanf(ptr, "%s", runtime);
+        
+        ctotal += atoi(cpoints);
+        printf("The pi approximated by child pid %s = %f\n", cpid, (double) 4 * atoi(cpoints) / atoi(spoints));
+        printf("The speedup time for child pid %s = %f\n\n", cpid, (double) (atoi(runtime) * 1e-9) / total_runtime);
     }
     
+    fclose(ptr);
+    
+    // Prints the pi approximatio using the total number of points in side the circle out of all the sample points.
     printf("Pi Approximation using number of points from all child processes = %f\n", (double) 4 * ctotal / NUM_POINT);
     return 0;
 }
