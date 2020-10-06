344194abe61f472bd584f3ea99b072a7d4ce365a
Asif Rasheed <b00073877@aus.edu>
2020-10-06 20:58:48 +0400

    Threads

---
+++ Threads/lab4.cpp
@@ -0,0 +1,27 @@
+// CMP 310 Lab 4
+#include<iostream>
+#include<QThread>
+using namespace std;
+
+class MyThread : public QThread{ // Class to implement run block for thread, similar approach to Java (Keep in mind that this is not the POSIX way of doing things)
+private:
+    int ID; // Thread ID
+    
+public:
+    MyThread(int id) : ID(id) {}
+    void run() { cout << "Thread " << ID << "is running\n"; }
+};
+
+int main(int argc, char** argv){
+    MyThread* threads[10];
+    
+    for(int i=0;i<10;i++){
+        threads[i] = new MyThread(i);
+        threads[i]->start(); // To start thread execution
+    }
+    
+    for(int i=0; i<10; i++)
+        threads[i]->wait(); // In order for the parent thread to wait for a child thread
+    
+    return 0;
+}
