6fd1dc78bc590c28bca917218f7c0c3be1003004
Asif Rasheed <b00073877@aus.edu>
2020-10-27 16:56:47 +0400

    ABC

--- Threads/ABC.cpp
+++ Threads/ABC.cpp
@@ -1,8 +1,8 @@
 #include<stdio.h>
-#include<QThreads>
+#include<QThread>
 #include<QSemaphore>
 
-class ThreadB : public QThreads{
+class ThreadB : public QThread{
 private:
     QSemaphore *semaphoreB, *semaphoreAC, *total;
 public:
@@ -10,11 +10,19 @@ public:
     ThreadB(QSemaphore *semaphoreB, QSemaphore *semaphoreAC, QSemaphore *total) : semaphoreB(semaphoreB), semaphoreAC(semaphoreAC), total(total) {}
     
     void run(){
-        
+        while(1){
+            semaphoreB->acquire();
+            if(!total->tryAcquire()){
+                semaphoreAC->release(2);
+                break;
+            }
+            printf("B");
+            semaphoreAC->release(4);
+        }
     }
 };
 
-class ThreadA : public QThreads{
+class ThreadA : public QThread{
 private:
     QSemaphore *semaphoreB, *semaphoreAC, *total;
 public:
@@ -22,11 +30,18 @@ public:
     ThreadA(QSemaphore *semaphoreB, QSemaphore *semaphoreAC, QSemaphore *total) : semaphoreB(semaphoreB), semaphoreAC(semaphoreAC), total(total) {}
     
     void run(){
-        
+        while(1){
+            semaphoreAC->acquire();
+            if(!total->tryAcquire()) break;
+            printf("A");
+            
+            if(semaphoreAC->available()==0 && semaphoreB->available() == 0)
+                semaphoreB->release();
+        }
     }
 };
 
-class ThreadC : public QThreads{
+class ThreadC : public QThread{
 private:
     QSemaphore *semaphoreB, *semaphoreAC, *total;
 public:
@@ -34,6 +49,33 @@ public:
     ThreadC(QSemaphore *semaphoreB, QSemaphore *semaphoreAC, QSemaphore *total) : semaphoreB(semaphoreB), semaphoreAC(semaphoreAC), total(total) {}
     
     void run(){
-        
+        while(1){
+            semaphoreAC->acquire();
+            if(!total->tryAcquire()) break;
+            printf("C");
+            
+            if(semaphoreAC->available()==0 && semaphoreB->available() == 0)
+                semaphoreB->release();
+        }
     }
 };
+
+int main(){
+    QSemaphore semaphoreB(1), semaphoreAC, total(100);
+    
+    ThreadA threadA(&semaphoreB, &semaphoreAC, &total);
+    ThreadB threadB(&semaphoreB, &semaphoreAC, &total);
+    ThreadC threadC(&semaphoreB, &semaphoreAC, &total);
+    
+    threadB.start();
+    threadA.start();
+    threadC.start();
+    
+    threadB.wait();
+    threadA.wait();
+    threadC.wait();
+    
+    printf("\n");
+    
+    return 0;
+}
